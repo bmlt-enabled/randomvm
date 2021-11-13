9ba3ee3d2a54283e2ac3df3ed0b1b2d09928cbfd
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-12 21:28:07 -0500

    make fresh request after 15min
    kiss
    
    Add return

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <svelte:head>
-    <link rel="manifest" href="manifest.json">
-    <link rel="apple-touch-icon" href="apple-icon-180.png">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="manifest" href="manifest.json" />
+    <link rel="apple-touch-icon" href="apple-icon-180.png" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <title>Random Virtual Meeting</title>
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" data-auto-a11y="true"></script>
 </svelte:head>
